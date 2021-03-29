ff13b3f74352913f80396cb2c168338e7d1e965c
Tomas Suchockas <tsuchockas@gmail.com>
2021-03-29 13:21:39 +0300

    Add project files.

---
+++ ValdymoSistema/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ValdymoSistema.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
