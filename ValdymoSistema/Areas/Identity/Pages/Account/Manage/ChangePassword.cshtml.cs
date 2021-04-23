a7666a688d94f1e2a8c4301b01e2589e8d12f4f3
Tomas Suchockas <tsuchockas@gmail.com>
2021-04-23 14:20:18 +0300

    Changed UI to LT

--- ValdymoSistema/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ ValdymoSistema/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -33,20 +33,20 @@ namespace ValdymoSistema.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = "Laukas privalomas")]
             [DataType(DataType.Password)]
             [Display(Name = "Current password")]
             public string OldPassword { get; set; }
 
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "Laukas privalomas")]
+            [StringLength(100, ErrorMessage = "{0} privalo būti tarp {2} ir {1} simbolių ilgumo.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "New password")]
+            [Display(Name = "Naujas slaptažodis")]
             public string NewPassword { get; set; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            [Compare("NewPassword", ErrorMessage = "Naujas slaptažodis nesutampa.")]
             public string ConfirmPassword { get; set; }
         }
 
@@ -55,7 +55,7 @@ namespace ValdymoSistema.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"Vartotojas nepasiekiamas su ID '{_userManager.GetUserId(User)}'.");
             }
 
             var hasPassword = await _userManager.HasPasswordAsync(user);
@@ -77,7 +77,7 @@ namespace ValdymoSistema.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"Vartotojas nepasiekiamas su ID '{_userManager.GetUserId(User)}'.");
             }
 
             var changePasswordResult = await _userManager.ChangePasswordAsync(user, Input.OldPassword, Input.NewPassword);
@@ -92,7 +92,7 @@ namespace ValdymoSistema.Areas.Identity.Pages.Account.Manage
 
             await _signInManager.RefreshSignInAsync(user);
             _logger.LogInformation("User changed their password successfully.");
-            StatusMessage = "Your password has been changed.";
+            StatusMessage = "Jūsų slaptažodis pakeistas";
 
             return RedirectToPage();
         }
--- ValdymoSistema/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ ValdymoSistema/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -67,7 +67,7 @@ namespace ValdymoSistema.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"Vartotojas nepasiekiamas su ID '{_userManager.GetUserId(User)}'.");
             }
 
             await LoadAsync(user);
@@ -79,7 +79,7 @@ namespace ValdymoSistema.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"Vartotojas nepasiekiamas su ID '{_userManager.GetUserId(User)}'.");
             }
 
             if (!ModelState.IsValid)
@@ -100,14 +100,14 @@ namespace ValdymoSistema.Areas.Identity.Pages.Account.Manage
                     protocol: Request.Scheme);
                 await _emailSender.SendEmailAsync(
                     Input.NewEmail,
-                    "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    "Patvirtinkite savo el. pašto adresą",
+                    $"Prašome patvirtiniti savo paskyrą spaudžiant <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>šią nuorodą</a>.");
 
-                StatusMessage = "Confirmation link to change email sent. Please check your email.";
+                StatusMessage = "Išsiųstas patvirtinimo laiškas. Prašome patikrinti jūsų pašto dežutę.";
                 return RedirectToPage();
             }
 
-            StatusMessage = "Your email is unchanged.";
+            StatusMessage = "Jūsų el. pašto adresas nepakeistas";
             return RedirectToPage();
         }
 
@@ -116,7 +116,7 @@ namespace ValdymoSistema.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"Vartotojas nepasiekiamas su ID '{_userManager.GetUserId(User)}'.");
             }
 
             if (!ModelState.IsValid)
@@ -136,10 +136,10 @@ namespace ValdymoSistema.Areas.Identity.Pages.Account.Manage
                 protocol: Request.Scheme);
             await _emailSender.SendEmailAsync(
                 email,
-                "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                "Patvirtinkite savo el. pašto adresą",
+                $"Prašome patvirtiniti savo paskyrą spaudžiant <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>šią nuorodą</a>.");
 
-            StatusMessage = "Verification email sent. Please check your email.";
+            StatusMessage = "Išsiųstas patvirtinimo laiškas. Prašome patikrinti jūsų pašto dežutę.";
             return RedirectToPage();
         }
     }
